7a0c6659e637ad5839be47aa1d23869a7dcc1a24
amazono <garikogannisyan.9@gmail.com>
2024-01-07 21:32:37 +0300

    div

--- tutor_js/script.js
+++ tutor_js/script.js
@@ -1,2 +1,23 @@
-const btn = document.querySelectorAll('test')
-console.log(btn)
+const div = document.querySelector('div');
+
+for (let i = 0; i < div.children.length; i ++) {
+    const elem = div.children[i];
+
+    if (elem.tagName === 'UL') {
+        elem.innerHTML = `
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+        `;
+    } else if(elem.tagName === 'SPAN') {
+        elem.textContent = 'Hello world';
+    }
+    console.log(elem)
+}
+
+// div.textContent = '<button>Hello world</button>'
+// div.innerHTML = 'Hello world'
+
+console.log(div.parentNode);
