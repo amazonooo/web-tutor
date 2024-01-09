7fcb85085a3b934960ae0dafdd97ffc647aedea8
amazono <garikogannisyan.9@gmail.com>
2024-01-09 16:29:50 +0300

    add

--- tutor_js/script.js
+++ tutor_js/script.js
@@ -1,4 +1,4 @@
-const div = document.querySelector('.element');
+// const div = document.querySelector('.element');
 
 // div.append('Hello');
 // div.prepend('Hello1');
@@ -46,19 +46,95 @@ const div = document.querySelector('.element');
 //     </ul>
 // `)
 
-const ul = document.createElement('ul')
+// const ul = document.createElement('ul')
 
-div.innerHTML = `
-    <ul>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-    </ul>
+// div.innerHTML = `
+//     <ul>
+//         <li>1</li>
+//         <li>2</li>
+//         <li>3</li>
+//     </ul>
+// `;
+
+// div.insertAdjacentHTML('afterend', `
+//     <ul>
+//         <li>4</li>
+//         <li>5</li>
+//     </ul>
+// `)
+
+// const ul = document.createElement('ul');
+
+// ul.innerHTML = `
+//     <ul>
+//         <li>1</li>
+//         <li>2</li>
+//         <li>3</li>
+//     </ul>
+// `;
+
+// const div = document.createElement('div');
+// div.textContent('darou')
+
+// const input = document.querySelector('[name="my_checkbox"]');
+// const a = document.querySelector('a');
+// // a.setAttribute('href', 'http://google.com');
+// // a.removeAttribute('href');
+// // console.log(a.hasAttribute('href'));
+// // a.setAttribute('my_custom_attr', 'privet');
+
+// // a.style.color = 'red';
+// // a.style.backgroundColor = 'blue';
+
+// // a.style.cssText = `
+// //     color: red;
+// //     background-color: black;
+// // `;
+
+// // const div = document.createElement('div');
+// // div.className = 'class_name';
+// // a.className = 'link2';
+
+// // a.classList.add('link2');
+// // a.classList.remove('link')
+
+// // console.log(a.classList.contains('link2'))
+// // console.log(a.classList);
+
+// // a.classList.toggle('active');
+
+// a.style.cssText = `
+//     background: black;
+// `;
+
+// aAttribute('href');
+
+// div.insertAdjacentHTML('afterbegin', `
+//     <ul>
+//         <li>1</li>
+//         <li>2</li>
+//     </ul>
+// `);
+
+
+// const button = document.createElement('button');
+// button.textContent = 'Our button';
+// button.style.cssText = `
+//     border-radius: 8px;
+//     background: transparent;
+//     padding: 4px 8px;
+//     border: 1px solid black; 
+// `;
+
+// document.body.append(button);
+
+const button = document.createElement('button');
+button.textContent = 'BUTTON';
+button.style.cssText = `
+    border-radius: 12px;
+    background: transparent;
+    padding: 8px 12px;
+    border: 1px solid aqua;
 `;
 
-div.insertAdjacentHTML('afterend', `
-    <ul>
-        <li>4</li>
-        <li>5</li>
-    </ul>
-`)
+document.body.append(button);
