aa1c63beb49b58100b25ff16589e7a9fe0ccfa56
amazono <garikogannisyan.9@gmail.com>
2024-02-04 21:50:23 +0300

    function

--- tut_js/script.js
+++ tut_js/script.js
@@ -1,17 +1,28 @@
-let age = 14;
-let question = prompt("Ваш возраст?");
-
-// if (question >= 18) {
-//     alert("Входите");
-// } else {
+// function myFirstApp(name, age) {
+//     alert(`Hello, my name is ${name} and it's my first programm`);
     
+//     function showSkills() {
+//         let skills = ['html', 'css', 'js'];
+//         for (let i = 0; i < skills.length; i++) {
+//             console.log(skills[i]); 
+//         }
+//     }
+//     showSkills();
+
+//    function checkAge(age) {
+//         if (age < 18) {
+//             console.log("У тебя отличные шансы!!!")
+//         } else {
+//             console.log('Пока рано!')
+//         }
+//     }
+//     checkAge();
+
+//     function calcPow(num) {
+//         return num**2;
+//     }
+//     console.log(calcPow(4));
 // }
 
-switch (age) {
-    case '18':
-        alert("Входите");
-        break;
-    case '100':
-        alert("нет");
-        break;
-}
+// myFirstApp('ivan', 19);
+
---
+++ tut_js/tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+    function calcPow(num) {
+        return num**2;
+    }
+    console.log(calcPow(4));
