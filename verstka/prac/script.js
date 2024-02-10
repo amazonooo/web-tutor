ad76246b4a20123fbaec796a6875d636e95c5e26
amazonooo <155751051+amazonooo@users.noreply.github.com>
2024-02-10 22:44:25 +0300

    fs

--- verstka/prac/script.js
+++ verstka/prac/script.js
@@ -3,6 +3,33 @@ const menuClose = document.querySelector('.menu__close');
 const headerList = document.querySelector('.header__list');
 const menuShadow = document.querySelector('.menu--close');
 
+const masterBtn = document.querySelector('.master-class');
+const reviewBtn = document.querySelector('.review');
+const contactBtn = document.querySelector('.contact');
+const reviews = document.querySelector('.reviews');
+const master = document.querySelector('.masters');
+const contact = document.querySelector('.subsribe')
+
+function scrollTo(elem) {
+    window.scroll({
+        left: 0,
+        top: elem.offsetTop,
+        behavior: 'smooth'
+    })
+}
+
+masterBtn.addEventListener('click', () => {
+    scrollTo(master);
+})
+
+reviewBtn.addEventListener('click', () => {
+    scrollTo(reviews);
+})
+
+contactBtn.addEventListener('click', () => {
+    scrollTo(contact);
+})
+
 menuBtn.addEventListener('click', () => {
     headerList.classList.toggle('header__list--open');
     menuShadow.classList.toggle('menu--open');
