1b878be2884d8aa952819810c419d466ce9491c6
GrigorUN <155751051+amazonooo@users.noreply.github.com>
2024-02-18 00:57:35 +0300

    hkj

--- verstka/tea/main.js
+++ verstka/tea/main.js
@@ -14,3 +14,49 @@ menuClose.addEventListener('click', ()=> {
 });
 
 
+const blackBtn = document.querySelector('.black');
+const blackImg = document.querySelector('.black-img');
+const greenBtn = document.querySelector('.green');
+const greenImg = document.querySelector('.green-img');
+const whiteBtn = document.querySelector('.white');
+const whiteImg = document.querySelector('.white-img');
+const oolongBtn = document.querySelector('.oolong');
+const oolongImg = document.querySelector('.oolong-img');
+const puBtn = document.querySelector('.pu-erh');
+const puImg = document.querySelector('.pu-erh-img');
+
+function scrollTo(elem) {
+    window.scroll({
+        behavior: 'smooth',
+        left: 0,
+        top: elem.offsetTop - 200
+    })
+}
+
+blackBtn.addEventListener('click', () => {
+    scrollTo(blackImg)
+    blackImg.classList.add('animation');
+})
+
+greenBtn.addEventListener('click', () => {
+    scrollTo(blackImg)
+    greenImg.classList.add('animation');
+})
+
+whiteBtn.addEventListener('click', () => {
+    scrollTo(blackImg)
+    whiteImg.classList.add('animation');
+})
+
+oolongBtn.addEventListener('click', () => {
+    scrollTo(blackImg)
+    oolongImg.classList.add('animation');
+})
+
+puBtn.addEventListener('click', () => {
+    scrollTo(blackImg)
+    puImg.classList.add('animation');
+})
+
+
+
