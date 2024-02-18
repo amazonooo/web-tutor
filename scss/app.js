a355d339c60ccff75d845262da3168c804a95ca5
GrigorUN <155751051+amazonooo@users.noreply.github.com>
2024-02-18 21:20:18 +0300

    df

---
+++ scss/app.js
@@ -0,0 +1,12 @@
+import './style.scss'
+
+function App() {
+    return (
+        <div className = 'App'>
+            <h1>SCSS basics</h1>
+            <button>Go to channel</button>
+        </div>
+    )
+}
+
+export default App
