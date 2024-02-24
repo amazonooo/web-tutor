fea9f1efe0b5447128bb6d6c2e0c19d5a30e7738
amazonooo <garikogannisyan.9@gmail.com>
2024-02-24 21:01:30 +0300

    asd

--- verstka/side/main.js
+++ verstka/side/main.js
@@ -1,16 +1,14 @@
 const nav = document.querySelector('.navigation');
 const toggle = document.querySelector('.menu__toggle');
-const list = document.querySelector('.list ');
+const list = document.querySelectorAll('.list');
 
 toggle.addEventListener('click', () => {
     nav.classList.toggle('open')
 })
 
 function activeLink() {
-    list.forEach((item) =>
-    item.classList.remove('active'));
-    this.classList.add('active');
+    list.forEach(item => item.classList.remove('active'))
+		this.classList.add('active')
 }
 
-list.forEach((item) =>
-item.addEventListener('click', activeLink))
+list.forEach(item => item.addEventListener('click', activeLink))
