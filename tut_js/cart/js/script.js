9d94833946e782ff39d7eb322ef5b74cdcb7542a
amazonooo <garikogannisyan.9@gmail.com>
2024-03-01 22:13:26 +0300

    par

--- tut_js/cart/js/script.js
+++ tut_js/cart/js/script.js
@@ -75,6 +75,59 @@
 // });
 
 
+// window.addEventListener('DOMContentLoaded', function () {
+//     let openBtn = document.querySelector('.open'),
+//         buttons = document.querySelectorAll('button'),
+//         products = document.querySelectorAll('.product');
+
+//     function createCart() {
+//         let cart = document.createElement('div'),
+//             heading = document.createElement('h2'),
+//             field = document.createElement('div'),
+//             closeBtn = document.createElement('button');
+
+//         cart.classList.add('cart');
+//         closeBtn.classList.add('close');
+//         field.classList.add('cart-field')
+
+//         heading.textContent = 'В нашей корзине:';
+//         closeBtn.textContent = 'Закрыть';
+
+//         document.body.appendChild(cart);
+//         cart.appendChild(heading);
+//         cart.appendChild(field);
+//         cart.appendChild(closeBtn);
+//     }
+
+//     createCart()
+
+//     let field = document.querySelector('.cart-field'),
+//         cart = document.querySelector('.cart'),
+//         close = document.querySelector('.close');
+
+//     function openCart() {
+//         cart.style.display = 'block';
+//     }
+
+//     function closeCart() {
+//         cart.style.display = 'none';
+//     }
+
+//     openBtn.addEventListener('click', openCart);
+//     close.addEventListener('click', closeCart);
+
+//     for (let i = 0; i < buttons.length; i++) {
+//         buttons[i].addEventListener('click', function() {
+//             let item = products[i].cloneNode(true),
+//                 btn = item.querySelector('button');
+
+//             btn.remove();
+//             field.appendChild(item);
+//             products[i].remove();
+//         });
+//     }
+// });
+
 window.addEventListener('DOMContentLoaded', function () {
     let openBtn = document.querySelector('.open'),
         buttons = document.querySelectorAll('button'),
@@ -117,13 +170,13 @@ window.addEventListener('DOMContentLoaded', function () {
     close.addEventListener('click', closeCart);
 
     for (let i = 0; i < buttons.length; i++) {
-        buttons[i].addEventListener('click', function() {
+        buttons[i].addEventListener('click', () => {
             let item = products[i].cloneNode(true),
-                btn = item.querySelector('button');
+                btn = item.querySelector('button')
 
             btn.remove();
             field.appendChild(item);
-            products[i].remove();
+            products[i].remove()
         });
     }
-});
+})
