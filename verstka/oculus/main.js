8f6fb4d9b39832164c0d48f7824b8654f8834a14
amazonooo <garikogannisyan.9@gmail.com>
2024-04-21 21:27:44 +0300

    sadf

--- verstka/oculus/main.js
+++ verstka/oculus/main.js
@@ -1,7 +1,80 @@
 const featuresLinks = document.querySelectorAll('.feutures__link')
+const menuLinks = document.querySelectorAll('.menu__link')
+const footerLinks = document.querySelectorAll('.footer__link');
 
-featuresLinks.forEach((item) => {
-    featuresLinks.addEventlistener('click', () => {
-			item.classlist.add('active')
+const shopBtn = document.querySelector('.shop')
+const shopSection = document.querySelector('.accessries')
+
+function scrollTo(elem) {
+	window.scroll({
+		left: 0,
+		// УЗНАЕМ ДЛИНУ БЛОКА
+		top: elem.offsetTop,
+		behavior: 'smooth',
+	})
+}
+
+shopBtn.addEventListener('click', () => {
+	scrollTo(shopSection)
+})
+
+featuresLinks.forEach(function(featuresLink) {
+  	featuresLink.addEventListener('click', function(event) {
+		event.preventDefault();
+		
+		featuresLinks.forEach(function(item) {
+		item.classList.remove('active');
+    });
+    
+    this.classList.add('active')
+	})
+})
+
+menuLinks.forEach(function(menuLink) {
+	menuLink.addEventListener('click', function(event) {
+		event.preventDefault();
+
+		menuLinks.forEach(function(item) {
+			item.classList.remove('active')
 		})
+
+		this.classList.add('active')
+	})
+})
+
+footerLinks.forEach(function(footerLink) {
+	footerLink.addEventListener('click', function(event) {
+		event.preventDefault();
+
+		footerLinks.forEach(function(item) {
+			item.classList.remove('active')
+		})
+
+		this.classList.add('active')
+	})
+})
+
+
+
+const products = document.querySelectorAll('.accesseries__card') // Получаем все товары
+
+console.log(products)
+
+products.forEach(function (product) {
+	product.addEventListener('click', function () {
+		const headerPosition =
+			document.querySelector('header').getBoundingClientRect().top +
+			window.scrollY // Получаем позицию header
+		const productPosition =
+			product.getBoundingClientRect().top + window.scrollY // Получаем позицию товара
+		const scrollDistance = headerPosition - productPosition // Вычисляем дистанцию для прокрутки
+
+		// Анимируем прокрутку
+		window.scrollTo({
+			top: scrollDistance,
+			behavior: 'smooth',
+		})
+
+		// Здесь можно добавить анимацию перемещения товара в корзину и обновление количества товаров
+	})
 })
